1bcc70da24640b8b9c77624ba84292344fd9af0f
Albéric de lajarte <albericlajarte@gmail.com>
2019-12-21 18:42:36 +0100

    added setup file

--- project.m
+++ project.m
@@ -1,8 +1,5 @@
 %% Setup
-addpath("/Users/Alberic/Downloads/casadi-osx-matlabR2015a-v3.5.1")
-
-cd /Library/gurobi811/mac64/matlab
-gurobi_setup;
+setup();
 
 %% Part 1
 quad = Quad();
---
+++ setup.m
@@ -0,0 +1,8 @@
+function setup()
+
+addpath("/Users/Alberic/Downloads/casadi-osx-matlabR2015a-v3.5.1")
+
+cd /Library/gurobi811/mac64/matlab
+gurobi_setup;
+
+end
