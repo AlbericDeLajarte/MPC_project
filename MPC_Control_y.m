218586a61c91037559b3b21f3918f42cf1ea3d36
Albéric de lajarte <albericlajarte@gmail.com>
2020-01-12 15:30:53 +0100

    Part 3_2 running and plotting

--- MPC_Control_y.m
+++ MPC_Control_y.m
@@ -20,7 +20,7 @@ classdef MPC_Control_y < MPC_Control
       us = sdpvar(m, 1);
       
       % SET THE HORIZON HERE
-      N = 15;  %aka 8sec /Ts, sachant que l'énoncé veut qu'on prenne 8 secondes pour remettre à l'origine le drone écarté de 2m sur x ou y
+      N = 20;  %aka 8sec /Ts, sachant que l'énoncé veut qu'on prenne 8 secondes pour remettre à l'origine le drone écarté de 2m sur x ou y
       
       % Predicted state and input trajectories
       x = sdpvar(n, N);
@@ -34,34 +34,10 @@ classdef MPC_Control_y < MPC_Control
       %       the DISCRETE-TIME MODEL of your system
 
       % WRITE THE CONSTRAINTS AND OBJECTIVE HERE
-      Q = eye(n); R = 2;
+      Q = eye(4); Q(3,3) = 6.3; Q(4,4) = 8.5;
+      R = 3.1;
       M = [1; -1]; m = [0.3; 0.3]; 
-      F = [0 1 0 0; 0 -1 0 0]; f = [0.035; 0.035];
-      
-      [K, Qf, ~] = dlqr(mpc.A, mpc.B, Q, R);
-      K = -K;
-      
-       % Compute maximal invariant set
-       Xf = polytope([F;M*K],[f;m]);
-
-    %   figure(3); plot(Xf.projection(3:4)); hold on;
-      
-       Acl =  mpc.A + mpc.B*K;
-       while 1
-           prevXf = Xf;
-           [T,t] = double(Xf);
-           preXf = polytope(T*Acl,t);
-           Xf = intersect(Xf, preXf);
-           if isequal(prevXf, Xf)
-               break
-           end
-     %      plot(Xf.projection(3:4)); hold on;
-           %pause;
-       end
-      [Ff,ff] = double(Xf);
-      
-   %   figure(4);plot(Xf.projection(3:4)); xlabel("beta angle"); ylabel("beta speed"); 
-    %  figure(5);plot(Xf.projection(1:2)); xlabel("x position"); ylabel("x speed"); 
+      F = [0 1 0 0; 0 -1 0 0]; f = [0.035; 0.035]; 
 
       con = (x(:,2)-xs == mpc.A*(x(:,1)-xs) + mpc.B*(u(1)-us)) + (M*(u(1)-us) <= m-M*us);
       obj = ((x(:,1)-xs)'*Q*(x(:,1)-xs))+(u(:,1)-us)'*R*(u(:,1)-us);
@@ -102,11 +78,7 @@ classdef MPC_Control_y < MPC_Control
       ref = sdpvar;
       
       % WRITE THE CONSTRAINTS AND OBJECTIVE HERE
-      
-      nx = size(mpc.A,1);
-      nu = size(mpc.B,2);
-      
-      Q = eye(n)*2; R = 1;
+      Q = 10;
       M = [1; -1]; m = [0.3; 0.3]; 
       F = [0 1 0 0; 0 -1 0 0]; f = [0.035; 0.035];      
 
@@ -114,7 +86,7 @@ classdef MPC_Control_y < MPC_Control
              F*xs <= f          ,...
              xs == mpc.A*xs + mpc.B*us ];
 
-      obj  = (mpc.C*xs - ref)'*(mpc.C*xs - ref);    
+      obj  = (mpc.C*xs - ref)'*Q*(mpc.C*xs - ref);    
       % Compute the steady-state target
       target_opt = optimizer(con, obj, sdpsettings('solver', 'gurobi'), ref, {xs, us});
       
